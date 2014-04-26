cbca0ff254fbea7b07d6ff3e1dd33e009fa0d978
Eric van Zanten <eric.vanzanten@gmail.com>
2014-04-25 19:50:49 -0500

    Getting junk setup

---
+++ README.md
@@ -0,0 +1,39 @@
+# WOPR Grid Explorer
+
+Explore geospatially aggregated WOPR datasets
+
+![Crime on a grid](https://dl.dropboxusercontent.com/u/11510165/wopr-grid.png)
+
+## Running locally
+
+``` bash
+git clone git@github.com:datamade/wopr-grid.git
+cd wopr-grid
+
+# to run locally
+python -m SimpleHTTPServer
+```
+
+navigate to http://localhost:8000/
+
+## Team
+
+* Derek Eder - developer, content
+* Eric van Zanten - developer, GIS data merging
+* Forest Gregg - process design, content
+
+## Errors / Bugs
+
+If something is not behaving intuitively, it is a bug, and should be reported.
+Report it here: https://github.com/datamade/chicago-lots/issues
+
+## Note on Patches/Pull Requests
+ 
+* Fork the project.
+* Make your feature addition or bug fix.
+* Commit, do not mess with rakefile, version, or history.
+* Send me a pull request. Bonus points for topic branches.
+
+## Copyright
+
+Copyright (c) 2014 DataMade. Released under the [MIT License](https://github.com/datamade/chicago-lots/blob/master/LICENSE).
---
+++ js/map.js
@@ -0,0 +1,34 @@
+(function(){
+    var map = L.map('map').fitBounds([[41.644286009999995, -87.94010087999999], [42.023134979999995, -87.52366115999999]]);
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/derekeder.hehblhbj/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    }).addTo(map);
+    var endpoint = 'http://127.0.0.1:5000/api'
+    var dataset = 'chicago_crimes_all';
+    var year = 2013
+    var resolution = 0.01;
+    $.when(getGrid(year, dataset, resolution)).then(
+        function(grid){
+            L.geoJson(grid, {
+                pointToLayer: function(feature, latlng){
+                    var size = feature.properties.count / 250;
+                    return  L.circleMarker(latlng).setRadius(size);
+                },
+                onEachFeature: function(feature, layer){
+                    var content = '<h4>Count: ' + feature.properties.count + '</h4>';
+                    layer.bindPopup(content, {
+                        closeButton: true,
+                        minWidth: 320
+                    })
+                }
+            }).addTo(map);
+        }
+    );
+    function getGrid(year, dataset, resolution){
+        var data = {
+            dataset_name: dataset,
+            resolution: resolution
+        }
+        return $.getJSON(endpoint + '/grid/' + year + '/', data)
+    }
+})()
