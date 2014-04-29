609ca5b25d7f308b7a9ef7143fa1533d029f9809
Eric van Zanten <eric.vanzanten@gmail.com>
2014-04-29 11:13:04 -0500

    Updated grid to be a bit more awesomer. Relates to datamade/wopr-api/#5

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
 (function(){
+    var grid_layer;
+    var jenks_cutoffs;
     var endpoint = 'http://wopr.datamade.us/api'
     var map = L.map('map').fitBounds([[41.644286009999995, -87.94010087999999], [42.023134979999995, -87.52366115999999]]);
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/datamade.hn83a654/{z}/{x}/{y}.png', {
@@ -8,9 +10,32 @@
         year: 2013,
         dataset: 'chicago_crimes_all',
         human_name: 'Crimes - 2001 to present',
-        resolution: 0.01,
+        resolution: 0.0111,
     }
-    var grid_layer;
+    var legend = L.control({position: 'bottomleft'});
+    legend.onAdd = function(map){
+        var div = L.DomUtil.create('div', 'legend')
+        var labels = [];
+        var from;
+        var to;
+        $.each(jenks_cutoffs, function(i, grade){
+            from = grade
+            to = jenks_cutoffs[i + 1];
+            labels.push('<i style="background:' + getColor(from) + '"></i>' +
+                       from + (to ? '&ndash;' + to : '+'));
+        });
+        div.innerHTML = '<div><strong>' + grid_data['human_name'] + '</strong><br />' + labels.join('<br />') + '</div>';
+        return div
+    }
+    var map_colors = [
+        '#deebf7',
+        '#c6dbef',
+        '#9ecae1',
+        '#6baed6',
+        '#4292c6',
+        '#2171b5',
+        '#084594'
+    ]
     metaUpdate(grid_data);
     loadLayer(grid_data);
     function loadLayer(grid_data){
@@ -19,25 +44,54 @@
         $.when(getGrid(url, grid_data['dataset'], grid_data['resolution'])).then(
             function(grid){
                 $('#map').spin(false);
+                var values = [];
+                $.each(grid['features'], function(i, val){
+                    values.push(val['properties']['count']);
+                });
+                jenks_cutoffs = jenks(values, 6);
+                jenks_cutoffs[0] = 0;
+                jenks_cutoffs.pop();
                 if (typeof grid_layer !== 'undefined'){
                     map.removeLayer(grid_layer);
                 }
                 grid_layer = L.geoJson(grid, {
                     pointToLayer: function(feature, latlng){
-                        var size = feature.properties.count / 250;
-                        return  L.circleMarker(latlng).setRadius(size);
+                        var res = grid_data['resolution'] / 2;
+                        var sw = [latlng.lat + res, latlng.lng - res]
+                        var ne = [latlng.lat - res, latlng.lng + res]
+                        var style = styleGrid(feature)
+                        return  L.rectangle([sw, ne], style);
                     },
                     onEachFeature: function(feature, layer){
                         var content = '<h4>Count: ' + feature.properties.count + '</h4>';
-                        layer.bindPopup(content, {
-                            closeButton: true,
-                            minWidth: 320
-                        })
+                        layer.bindLabel(content);
                     }
                 }).addTo(map);
                 $('#dataset-name').text(grid_data['human_name']);
+                try{legend.removeFrom(map);}catch(e){};
+                legend.addTo(map);
             }
         );
+    }
+
+    function getColor(d){
+        return d >= jenks_cutoffs[5] ? map_colors[6] :
+               d >= jenks_cutoffs[4] ? map_colors[5] :
+               d >= jenks_cutoffs[3] ? map_colors[4] :
+               d >= jenks_cutoffs[2] ? map_colors[3] :
+               d >= jenks_cutoffs[1] ? map_colors[2] :
+               d >= jenks_cutoffs[0] ? map_colors[1] :
+                                       map_colors[0];
+    }
+
+    function styleGrid(feature){
+        return {
+            fillColor: getColor(feature.properties.count),
+            weight: 0.3,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 0.7
+        };
     }
 
     function getGrid(url, dataset, resolution){
@@ -47,6 +101,7 @@
         }
         return $.getJSON(url, data)
     }
+
     function metaUpdate(grid_data){
         $('.meta').spin('large');
         var tpl = new EJS({text: $('#metaControl').html()})
